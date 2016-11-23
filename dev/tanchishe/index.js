babcbdbc4b2b46534155cfad0f8efa6a05ab11ae
unknown <zhang_zao@ccw.lab>
2016-11-23 09:45:06 +0800

    js

--- dev/tanchishe/index.js
+++ dev/tanchishe/index.js
@@ -2,17 +2,31 @@ $(document).ready(function(){
 	// 创建场地
 	var tablecont = $('#container');
 	var atr = "<tr></tr>";
-	var atd = "<td></td>";
+	var atd = "<td data-coord=''></td>";
 	for (let i = 0; i < 50; i++) {
 		tablecont.append(atr);
 		for (let j = 0; j < 100; j++) {
 			var lastchild = tablecont.children('tr:last');
 			lastchild.append(atd);
+			lastchild.children('td:last').attr('data-coord',i+','+j);
 		}
 	}
 
 	function SNAKE(){
-		this.lenght = '5';
-		this.dir = '0';
+		this.arr = Array();
+		// this.length = '5';
+		// 方向2,4,6,8
+		this.dir = 6;
 	}
+
+	// 24,49
+	var smallsnake = new SNAKE();
+	var snakehead = $("td[data-coord='24,49']");
+	smallsnake.arr.push(snakehead);
+	console.log(snakehead.attr('data-coord'));
+
+	if(smallsnake.dir == 6){
+
+	}
+	console.log(smallsnake.arr);
 })
