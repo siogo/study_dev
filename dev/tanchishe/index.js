67703988872924a19e4edea67d9cf5db043a91fa
unknown <zhang_zao@ccw.lab>
2016-11-23 17:17:49 +0800

    no message

--- dev/tanchishe/index.js
+++ dev/tanchishe/index.js
@@ -13,17 +13,16 @@ $(document).ready(function(){
 	}
 
 	function SNAKE(){
-		this.arr = Array();
-		// this.length = '5';
+		this.length = 5;
 		// 方向2,4,6,8
 		this.dir = 6;
 	}
 
 	// 24,49
+	// 蛇初始化
 	var smallsnake = new SNAKE();
 	var snakehead = $("td[data-coord='24,49']");
 	snakehead.attr("data-head",'yes');
-	smallsnake.arr.push(snakehead);
 	snakehead.css("background-color", '#000');
 	console.log(snakehead.attr('data-coord'));
 
@@ -32,21 +31,30 @@ $(document).ready(function(){
 			$("td[data-coord='24,"+i+"']").css("background-color","#000");
 		}
 	}
-	console.log(smallsnake.arr);
-	var runTime = setInterval(function(){
-		$('td').css("background-color","#fff");
-		let runCoord = $("td[data-head='yes']").attr('data-coord');
-		let splitStr = runCoord.split(',');
-		let coordAbs = parseInt(splitStr[0]);
-		let coordOrd = parseInt(splitStr[1]);
-		coordOrd = coordOrd+1;
-		$("td[data-head='yes']").removeAttr("data-head");
-		if(smallsnake.dir == 6){
-			$('td').css("background-color","#fff");
-			for (let i = coordOrd; i > coordOrd-5; i--) {
-				$("td[data-coord='"+coordAbs+","+i+"']").css("background-color","#000");
-			}
-			$("td[data-coord='"+coordAbs+","+coordOrd+"']").attr("data-head",'yes');
+
+	// var runTime = setInterval(function(){
+	// 	$('td').css("background-color","#fff");
+	// 	let runCoord = $("td[data-head='yes']").attr('data-coord');
+	// 	let splitStr = runCoord.split(',');
+	// 	let coordAbs = parseInt(splitStr[0]);
+	// 	let coordOrd = parseInt(splitStr[1]);
+	// 	coordOrd = coordOrd+1;
+	// 	$("td[data-head='yes']").removeAttr("data-head");
+	// 	if(smallsnake.dir == 6){
+	// 		$('td').css("background-color","#fff");
+	// 		for (let i = coordOrd; i > coordOrd-5; i--) {
+	// 			$("td[data-coord='"+coordAbs+","+i+"']").css("background-color","#000");
+	// 		}
+	// 		$("td[data-coord='"+coordAbs+","+coordOrd+"']").attr("data-head",'yes');
+	// 	}
+	// },1000);
+
+
+// 37zuo  39you  38shang  40xia
+	$(document).keydown(function(event){
+		if(event.keyCode == 37){
+
 		}
-	},1000);
+		console.log(event.keyCode);
+	});
 })
