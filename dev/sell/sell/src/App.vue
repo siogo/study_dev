a04bc62fc5c840309721cb639177b40fcd8eec25
unknown <zhang_zao@ccw.lab>
2017-03-29 16:40:48 +0800

    20170329 title data

--- dev/sell/sell/src/App.vue
+++ dev/sell/sell/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab test">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -19,8 +19,23 @@
 
 <script>
   import header from './components/header/header.vue'
+  const ERR_OK = 0;
 
   export default {
+    data(){
+      return {
+        seller: {}
+      }
+    },
+    created(){
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if(response.errno === ERR_OK){
+          this.seller = response.data;
+          console.log(this.seller);
+        }
+      });
+    },
     components:{
       'v-header':header
     }
@@ -58,6 +73,9 @@
   .tab-item{
     flex: 1;
     text-align: center;
+  }
+  .tab-item .router-link-active{
+    color: rgb(240,20,20);
   }
 
 
