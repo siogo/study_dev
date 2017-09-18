b2512d98ce66ecbadcb090f70df1d797e3a1bd97
unknown <zhang_zao@ccw.lab>
2017-09-18 09:52:50 +0800

    test node

--- dev/node/server.js
+++ dev/node/server.js
@@ -1,11 +1,37 @@
 var http = require('http');
-// console.log(http);
+var events = require('events');
+var zlib = require('zlib');
+var fs = require('fs');
+var url = require('url');
+// console.log(events);
+
+fs.createReadStream('input.txt').pipe(fs.createWriteStream('output.txt'));
+
+fs.createReadStream('input.txt').pipe(zlib.createGzip()).pipe(fs.createWriteStream('input.zip'))
+
+
+var eventEmitter = new events.EventEmitter();
+
+eventEmitter.on('test1',function(){
+    console.log('this is test1-1');
+})
+
+eventEmitter.addListener('test1',function(){
+    console.log('this is test1-2');
+})
+
+eventEmitter.emit('test1');
+eventEmitter.emit('test2');
+
+console.log(events.listenerCount(eventEmitter,'test1'))
 
 function start(){
     http.createServer(function(req,res){
-        console.log('request received');
+        // console.log('request received');
+        var pathname = url.parse(req.url).pathname;
         res.writeHead(200,{"Content-Type":"text/plain"});
         res.write('hello node');
+        res.write('pathname:'+pathname);
         res.end();
     }).listen(5454);
     console.log("server has started")
