332db4fd5c15afc358bf8a0fbe1e07b9fd1e780a
cheung <zz@daSliver.local>
2017-10-09 22:14:26 +0800

    EPSON

--- dev/node/Epson/js/excel.js
+++ dev/node/Epson/js/excel.js
@@ -1,12 +1,14 @@
 var xlsx = require('node-xlsx');
 var fs = require('fs');
+var path = require('path')
 
 var obj = xlsx.parse('../input/F9380.xlsx');
 var excelObj = obj[0].data;
+var str = '';
 var smallpoint;
 var keyspecIndex;
-for(var i = 0;i<excelObj.length;i++){
-    console.log(excelObj[i]);
+var keyspecs = [];
+for(let i = 0;i<excelObj.length;i++){
     if(excelObj[i][0] == '概述小点'){
         smallpoint = excelObj[i];
     }
@@ -14,6 +16,33 @@ for(var i = 0;i<excelObj.length;i++){
         keyspecIndex = i;
     }
 }
-console.log(keyspecIndex);
+outermost:
+for(let j = keyspecIndex+1; j<30;j++){ 
+    if(excelObj[j][0] == undefined){
+        if(excelObj[j][2] == undefined){
 
-// console.log(smallpoint);
+        }else{
+            str = str + excelObj[j][1]+':'+excelObj[j][2]+`\n`;
+            // keyspecs.push(excelObj[j][1]+':'+excelObj[j][2]);
+        }
+    }else{
+        break outermost;
+    }
+    
+}
+fs.writeFile(path.join(__dirname,'../input/input.txt'),smallpoint[1].trim(),function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log(`input写入成功`);
+    }
+})
+
+fs.writeFile(path.join(__dirname,'../input/keyspec.txt'),str.trim(),function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(`keyspce写入成功`)
+    }
+})
+
